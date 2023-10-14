6593c193a6122e29594cda79b927464088c28a44
akashi <mengxin@dduu.work>
2023-10-14 15:35:36 +0800

    feat: AddNewSmallEmotion 38

---
+++ app/src/main/java/cc/ioctl/hook/experimental/AddNewEmotion.java
@@ -0,0 +1,85 @@
+/*
+ * QAuxiliary - An Xposed module for QQ/TIM
+ * Copyright (C) 2019-2023 QAuxiliary developers
+ * https://github.com/cinit/QAuxiliary
+ *
+ * This software is non-free but opensource software: you can redistribute it
+ * and/or modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version and our eula as published
+ * by QAuxiliary contributors.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * and eula along with this software.  If not, see
+ * <https://www.gnu.org/licenses/>
+ * <https://github.com/cinit/QAuxiliary/blob/master/LICENSE.md>.
+ */
+package cc.ioctl.hook.experimental;
+
+import static io.github.qauxv.util.HostInfo.requireMinQQVersion;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import cc.ioctl.util.HookUtils;
+import io.github.qauxv.base.annotation.FunctionHookEntry;
+import io.github.qauxv.base.annotation.UiItemAgentEntry;
+import io.github.qauxv.dsl.FunctionEntryRouter;
+import io.github.qauxv.hook.CommonSwitchFunctionHook;
+import io.github.qauxv.util.Initiator;
+import io.github.qauxv.util.QQVersion;
+import io.github.qauxv.util.SyncUtils;
+import io.github.qauxv.util.dexkit.CPopOutEmoticonUtil;
+import io.github.qauxv.util.dexkit.DexKit;
+import io.github.qauxv.util.dexkit.DexKitTarget;
+import io.github.qauxv.util.dexkit.EmoticonHandler_handleSmallEmotion;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Objects;
+
+@FunctionHookEntry
+@UiItemAgentEntry
+public class AddNewEmotion extends CommonSwitchFunctionHook {
+    private AddNewEmotion() {
+        super(SyncUtils.PROC_MAIN, new DexKitTarget[]{EmoticonHandler_handleSmallEmotion.INSTANCE});
+    }
+
+    @NonNull
+    @Override
+    public String getName() {
+        return "添加38号小表情";
+    }
+
+    @Nullable
+    @Override
+    public String getDescription() {
+        return "仅在本地生效";
+    }
+
+    @NonNull
+    @Override
+    public String[] getUiItemLocation() {
+        return FunctionEntryRouter.Locations.Auxiliary.EXPERIMENTAL_CATEGORY;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return requireMinQQVersion(QQVersion.QQ_8_9_28);
+    }
+
+    public static final AddNewEmotion INSTANCE = new AddNewEmotion();
+
+    @Override
+    public boolean initOnce() throws Exception {
+        Method method = DexKit.requireMethodFromCache(EmoticonHandler_handleSmallEmotion.INSTANCE);
+        HookUtils.hookBeforeIfEnabled(this, method, param -> {
+            ArrayList<String> aList = (ArrayList) param.args[4];
+            aList.add("38");
+        });
+        return true;
+    }
+}
