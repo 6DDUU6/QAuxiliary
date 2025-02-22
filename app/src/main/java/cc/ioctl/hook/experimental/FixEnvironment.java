52d7a2db6949620c56de012b86f3db2c77e1d3cd
akashi <mengxin@dduu.work>
2025-02-22 17:19:23 +0800

    fix

--- app/src/main/java/cc/ioctl/hook/experimental/FixEnvironment.java
+++ app/src/main/java/cc/ioctl/hook/experimental/FixEnvironment.java
@@ -1,21 +1,26 @@
 package cc.ioctl.hook.experimental;
 
+import static io.github.qauxv.util.HostInfo.getHostInfo;
 import static io.github.qauxv.util.HostInfo.requireMinQQVersion;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import cc.ioctl.util.HookUtils;
 import io.github.qauxv.base.annotation.FunctionHookEntry;
 import io.github.qauxv.base.annotation.UiItemAgentEntry;
 import io.github.qauxv.dsl.FunctionEntryRouter;
 import io.github.qauxv.hook.CommonSwitchFunctionHook;
 import io.github.qauxv.util.QQVersion;
 import io.github.qauxv.util.SyncUtils;
+import java.io.File;
+import java.lang.reflect.Method;
 
 @FunctionHookEntry
 @UiItemAgentEntry
 public class FixEnvironment extends CommonSwitchFunctionHook {
+
     private FixEnvironment() {
-        super(null, true, null, SyncUtils.PROC_ANY);
+        super(null, true, null, SyncUtils.PROC_MAIN | SyncUtils.PROC_MSF);
     }
 
     @NonNull
@@ -45,6 +50,18 @@ public class FixEnvironment extends CommonSwitchFunctionHook {
 
     @Override
     public boolean initOnce() throws Exception {
+        Method method = File.class.getDeclaredMethod("canRead");
+        HookUtils.hookAfterIfEnabled(this, method, param -> {
+            File file = (File) param.thisObject;
+            if (
+                    file.toString().equals("\\data\\data\\" + getHostInfo().getPackageName() + "\\..") ||
+                            file.toString().equals("/data/data/" + getHostInfo().getPackageName() + "/..") ||
+                            file.toString().equals("/data/data/") ||
+                            file.toString().equals("/data/data")
+            ) {
+                param.setResult(false);
+            }
+        });
         nativeInitEnvironmentHook();
         return true;
     }
